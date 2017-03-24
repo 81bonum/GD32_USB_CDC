1ac2003699130d1619faaf47d2766a6a2a72502c
Illationov Maksim <morphyyyy@gmail.com>
2017-03-24 16:49:45 +0500

    изменение инициализации

--- Project/USB_Device_Examples/VCP/MDK-ARM/inc/can_init.h
+++ Project/USB_Device_Examples/VCP/MDK-ARM/inc/can_init.h
@@ -1,65 +1,47 @@
-/**
-  *****************************************************************************
-  * @title		can.h
-  * @platform	STM32F103
-  * @author		Dmitry Ovcharenko (https://www.smartmode.info/)
-  * @version	V1.0.0
-  * @date		20.06.2016
-  *
-  * @brief		???????????? ???? + ?????? ??????
-  *
-  *******************************************************************************
-  * @attention
-  *
-  *
-  * <h2><center>&copy; COPYRIGHT 2016 SmartMODE</center></h2>
-*/
-
-
 #ifndef __CAN
 #define __CAN
 
 
 /******************************************************************************
- * ??????????? ????????? CAN
+ * Заголовочник CAN
  ******************************************************************************/
+ #include "shell.h"
+ 
+ 
 //#define MODE CAN_MODE_SILENT_LOOPBACK
 //#define MODE CAN_MODE_SILENT
 //#define MODE CAN_MODE_LOOPBACK
 #define MODE CAN_MODE_NORMAL
  
-#define CAN1_ReMap // ???????????????, ???? ??? ????????? ??????
+#define CAN1_ReMap // Переназначение CAN1 на нужные выводы
 
 #ifndef CAN1_ReMap
 	#define CAN1_GPIO_PORT			GPIOA
-	#define CAN1_RX_SOURCE			GPIO_PIN_11				// RX-????
-	#define CAN1_TX_SOURCE			GPIO_PIN_12				// TX-????
-	#define CAN1_Periph				RCC_APB2PERIPH_GPIOA	// ???? ?????????
+	#define CAN1_RX_SOURCE			GPIO_PIN_11				// RX
+	#define CAN1_TX_SOURCE			GPIO_PIN_12				// TX
+	#define CAN1_Periph				RCC_APB2PERIPH_GPIOA	// Тактирование блока переферии A
 #else
 	#define CAN1_GPIO_PORT			GPIOB
-	#define CAN1_RX_SOURCE			GPIO_PIN_8				// RX-????
-	#define CAN1_TX_SOURCE			GPIO_PIN_9				// TX-????
-	#define CAN1_Periph				RCC_APB2PERIPH_GPIOB	// ???? ?????????
+	#define CAN1_RX_SOURCE			GPIO_PIN_8				// RX
+	#define CAN1_TX_SOURCE			GPIO_PIN_9				// TX
+	#define CAN1_Periph				RCC_APB2PERIPH_GPIOB	// Тактирпование блока переферии B
 #endif
 
-extern int getSpeed();
 
-// ????? ???????? ????
-// ?????????? ????????????????? ?????? ??????
 // #define CAN1_SPEED_PRESCALE			2						// 750 Kb
 // #define CAN1_SPEED_PRESCALE			3					  // 500 Kb
-//	 #define CAN1_SPEED_PRESCALE	 		5						// 300 Kb
+ #define CAN1_SPEED_PRESCALE	 		5						// 300 Kb
 // #define CAN1_SPEED_PRESCALE			10					// 150 Kb
 // #define CAN1_SPEED_PRESCALE			15					// 100 Kb
 // #define CAN1_SPEED_PRESCALE			30					// 50 Kb
 // #define CAN1_SPEED_PRESCALE			75					// 20 Kb
 // #define CAN1_SPEED_PRESCALE			150					// 10 Kb
-#define CAN1_SPEED_PRESCALE getSpeed();
+// #define CAN1_SPEED_PRESCALE getSpeed();
 
 
 
 /******************************************************************************
- * ??????????? ?????? ?????????
+ * 
  ******************************************************************************/
 #define CAN_CMD_Test_Send			0x0001		// ??????? ???????? ????????? ?????????
 #define CAN_CMD_Test_Ok				0x0002		// ??????? ????????????? ????????? ?????????
@@ -68,7 +50,7 @@ extern int getSpeed();
 
 
 /******************************************************************************
- * ??????????? ?????????? ???????
+ * 
  ******************************************************************************/
 void init_CAN(void);
 void CAN1_RX0_IRQHandler(void);
